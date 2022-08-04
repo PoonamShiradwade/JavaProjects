bba0e756d42e9093b6fab676d848c3b59cc6c261
PoonamShiradwade <110108866+PoonamShiradwade@users.noreply.github.com>
2022-08-04 13:36:17 +0530

    Add files via upload

---
+++ Conacat.java
@@ -0,0 +1,16 @@
+package practice5;
+
+public class Conacat {
+
+	public static void main(String[] args) 
+	{
+		String str1="Poonam";
+		String str2="Siradwade";
+		String str3="Shiraguppi";
+		System.out.println(str1.concat(str2).concat(str3));
+		
+		
+
+	}
+
+}
